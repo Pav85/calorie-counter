a071f402decba0d8f19f0ce351d43f17d86d0706
Pawel <pawel.werbowy@gmail.com>
2024-03-25 11:30:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -59,6 +59,10 @@ function calculateCalories(e) {
   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
   const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
   const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+
+  if (isError) {
+    return;
+  }
 }
 
 function getCaloriesFromInputs(list) {
